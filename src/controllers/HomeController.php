03a3fdc087737c4856326bc5c2576c03f691dfe1
andrey <andreyhideki@gmail.com>
2023-03-18 02:00:19 -0300

    add getall and get in homecontroller

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -3,8 +3,13 @@
 namespace src\controllers;
 
 use core\Controller;
+use PDO;
 
 class HomeController extends Controller {
+
+    // private function __construct() { 
+
+    // }
 
     public function index() {
         echo("index");
@@ -20,11 +25,111 @@ class HomeController extends Controller {
         print_r($args);
     }
 
-    public function getAll(){
-        echo("GETALL");
-    }
-
     public function ping(){
         echo("PONG");
+    }
+
+    public function getall(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "";
+        
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        $array = [
+            'error' => '',
+            'result' => []
+        ];
+
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $methodDefault = 'get';
+
+        if($method === $methodDefault)
+        {
+            $sql = $pdo->query("SELECT * FROM poke.pokemon");
+            if($sql->rowCount() > 0)
+            {
+                $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach($data as $item)
+                {
+                    $array['result'][] = [
+                        'id' => $item['id'],
+                        'name' => $item['name'],
+                        'description' => $item['description']
+                    ];
+                }
+            }
+        }
+        else
+        {
+            $array['error'] = 'Método não permitido';
+        }
+
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Content-Type: application/json");
+        echo json_encode($array);
+    }
+
+    public function get(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "";
+        
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        $array = [
+            'error' => '',
+            'result' => []
+        ];
+
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $methodDefault = 'get';
+
+        if($method === $methodDefault)
+        {
+
+            $id = filter_input(INPUT_GET, 'id');
+
+            if($id)
+            {
+                $sql = $pdo->prepare('SELECT * FROM poke.pokemon where id = :id');
+                $sql->bindValue(':id', $id);
+                $sql->execute();
+
+                if($sql->rowCount() > 0)
+                {
+                    $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+                    foreach($data as $item){
+                        $array['result'][] = [
+                            'id' => $item['id'],
+                            'name' => $item['name'],
+                            'description' => $item['description']
+                        ];
+                    }
+                }
+                else
+                {
+                    $array['error'] = 'Registro não existente!';
+                }
+            }
+            else
+            {
+                $array['error'] = 'Código obrigatório!';
+            }
+        }
+        else
+        {
+            $array['error'] = 'Método não permitido';
+        }
+
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Content-Type: application/json");
+        echo json_encode($array);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -7,3 +7,9 @@ $router->get('/', 'HomeController@index');
 $router->get('/sobre/{nome}', 'HomeController@sobreP');
 $router->get('/sobre', 'HomeController@sobre');
 $router->get('/ping', 'HomeController@ping');
+
+$router->get('/get', 'HomeController@get');
+$router->get('/getall', 'HomeController@getall');
+// $router->put('/update', 'HomeController@update');
+// $router->post('/insert', 'HomeController@insert');
+// $router->delete('/delete', 'HomeController@delete');
